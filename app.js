f096db8c047b0dc271546e8394390c16fae4939a
ramirosoto <sotoramiro302yahoo.com>
2019-12-10 12:25:51 -0800

    commit ramiro

--- app.js
+++ app.js
@@ -3,14 +3,43 @@ const app = express();
 const spoonacular = "https://api.spoonacular.com/recipes/";
 const API_KEY = "17ad2cf054384d1ca2800726be7bf110";
 const unirest = require('unirest');
+const bodyparser = require('body-parser');
+const mysql = require('mysql');
+const session = require('express-session');
+//Enable sessions
+app.use(session({
+    secret: 'secret',
+    path: '/',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+//mysql connection
+const con = mysql.createConnection({
+   host: 'mcldisu5ppkm29wf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+   user: 'vp4onqntwtnhnl44',
+   password:'eqgdpotw2735k904',
+   database:'oww96xtpilfl3guz'
+});
+con.connect((err)=>{
+    if (err){
+        console.log('failed to connect to database');
+    }
+    console.log('Connected to database');
+});
+global.con = con;
 
 app.set("view engine", "ejs");
+app.set('views', __dirname + '/views');
 
 app.use(express.static("public")); //access images, css, js
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true}));
 
 // enable use of json
 app.use(express.json());
@@ -18,11 +47,15 @@ app.use(express.urlencoded({
     extended: false
 }));
 
-// routes
+//loginroutes
+  const {logInPage, logInPost, RegisterPost} = require('./routes/loginroute');
+  app.get("/login", logInPage);
+  app.post("/login", logInPost);
+  app.post("/register", RegisterPost);
+
+  // routes
 app.get("/", function(req, res) {
-
     res.render("index");
-
 });
 
 app.get("/searchByIngredient", function(req, res, next){
